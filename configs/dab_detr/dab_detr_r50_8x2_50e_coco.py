d58ec2e3ccbbc1e43cce499345f99c47643a7565
LYMDLUT <70597027+LYMDLUT@users.noreply.github.com>
2022-08-07 00:40:56 +0800

    Update dab_detr_r50_8x2_50e_coco.py

--- configs/dab_detr/dab_detr_r50_8x2_50e_coco.py
+++ configs/dab_detr/dab_detr_r50_8x2_50e_coco.py
@@ -181,8 +181,8 @@ optimizer = dict(
     weight_decay=0.0001,
     paramwise_cfg=dict(
         custom_keys={'backbone': dict(lr_mult=0.1, decay_mult=1.0)}))
-optimizer_config = dict(grad_clip=dict(max_norm=0.1, norm_type=2))
-#optimizer_config = dict(grad_clip=dict(max_norm=0.1, norm_type=2),detect_anomalous_params=True)
+#optimizer_config = dict(grad_clip=dict(max_norm=0.1, norm_type=2))
+optimizer_config = dict(grad_clip=dict(max_norm=0.1, norm_type=2),detect_anomalous_params=True)
 # learning policy
 lr_config = dict(policy='step', step=[40])
 runner = dict(type='EpochBasedRunner', max_epochs=50)
